6f41209ebbf99ee5744e074eba425416e4703688
Jennifer Baik <sbaik058@gmail.com>
2020-01-19 18:25:45 +1300

    Use scope bindings for workflow such as pending review

---
+++ GameCore.Specs/AwaitingReviewSteps.cs
@@ -0,0 +1,18 @@
+﻿using TechTalk.SpecFlow;
+
+namespace GameCore.Specs
+{
+    [Binding]
+    [Scope(Tag = "awaitingReviewBeforeStartingWork")]
+    public class AwaitingReviewSteps
+    {
+        [Given (".*")]
+        [When (".*")]
+        [Then (".*")]
+        //using regex that is going to match anything
+        public void Empty()
+        {
+
+        }
+    }
+}
